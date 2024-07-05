cc2fe010c92fac77eb52b9fef84f8918e7e3bd37
Mathias Ramilo <mathiramilo2290@gmail.com>
2024-07-05 02:39:30 -0300

    Layout and styles finished. Features started

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,188 @@
-import Image from "next/image";
+'use client'
+
+import Image from 'next/image'
+import { useCallback, useState } from 'react'
+
+import { debounce } from '@/utils/debounce'
+
+const TRANSLATION_EXAMPLES = {
+  gn: [
+    'Traducción de texto de ejemplo 1',
+    'Traducción de texto de ejemplo 2',
+    'Traducción de texto de ejemplo 3',
+    'Traducción de texto de ejemplo 4',
+    'Traducción de texto de ejemplo 5'
+  ],
+  es: [
+    "Ñembohasa ha'e ñe'ẽme 1",
+    "Ñembohasa ha'e ñe'ẽme 2",
+    "Ñembohasa ha'e ñe'ẽme 3",
+    "Ñembohasa ha'e ñe'ẽme 4",
+    "Ñembohasa ha'e ñe'ẽme 5"
+  ]
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [originLanguage, setOriginLanguage] = useState('es')
+  const [originText, setOriginText] = useState<string>('')
+  const [translatedText, setTranslatedText] = useState<string>('')
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+  const toggleLanguage = () => {
+    setOriginLanguage(originLanguage === 'es' ? 'gn' : 'es')
+    setOriginText(translatedText)
+    setTranslatedText(originText)
+  }
+
+  const translate = (text: string, src: string) => {
+    // API call to translate text
+
+    if (!text) {
+      setTranslatedText('')
+      return
+    }
+
+    // Random number between 0 and 4
+    const randomIndex = Math.floor(Math.random() * 5)
+
+    // Set translated text
+    setTranslatedText(TRANSLATION_EXAMPLES[src][randomIndex])
+  }
+
+  const debouncedChange = useCallback(
+    debounce((text: string) => translate(text, originLanguage), 500),
+    []
+  )
+
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      {/* Background */}
+      <div className="w-screen h-screen absolute -z-50">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/bg-image.svg"
+          height={1920}
+          width={1080}
+          alt="World Map"
+          className="object-cover w-full h-full"
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {/* Copyright */}
+      <p className="absolute bottom-4 text-white/50 text-sm font-light">© 2024 Facultad de Ingenieria UDELAR</p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      {/* Translator */}
+      <section className="flex flex-col items-center justify-center gap-6 sm:gap-10 w-[90%] max-w-5xl">
+        {/* Swap Languages */}
+        <div className="flex items-center justify-center gap-6 sm:gap-10 w-full">
+          {/* Origin language */}
+          <div className="flex items-center justify-start gap-4 px-4 py-2 w-40 bg-white rounded-full border-[1px] border-gray-300">
+            <Image
+              src={originLanguage === 'es' ? '/spain-flag.svg' : '/paraguay-flag.svg'}
+              height={30}
+              width={30}
+              alt="Spain Flag"
+              className="border-[1px] border-gray-300 rounded-full p-[1px]"
+            />
+            <p className="font-medium">{originLanguage === 'es' ? 'Español' : 'Guarani'}</p>
+          </div>
+          {/* Swap button */}
+          <button
+            className="bg-black rounded-full p-2 hover:scale-105 transition-all"
+            onClick={() => toggleLanguage()}
+          >
+            <Image
+              src="/icons/refresh.svg"
+              height={32}
+              width={32}
+              alt="Swap"
+            />
+          </button>
+          {/* Dest. Language */}
+          <div className="flex items-center justify-start gap-4 px-4 py-2 w-40 bg-white rounded-full border-[1px] border-gray-300">
+            <Image
+              src={originLanguage === 'es' ? '/paraguay-flag.svg' : '/spain-flag.svg'}
+              height={30}
+              width={30}
+              alt="Paraguay Flag"
+              className="border-[1px] border-gray-300 rounded-full p-[1px]"
+            />
+            <p className="font-medium">{originLanguage === 'es' ? 'Guarani' : 'Español'}</p>
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        {/* Translation Boxes */}
+        <div className="grid sm:grid-cols-2 gap-4 sm:gap-8 w-full">
+          {/* Origin Text */}
+          <div className="flex flex-col gap-4 p-6 bg-white border-[1px] border-gray-300 rounded-2xl">
+            <textarea
+              className="h-32 sm:h-48 focus:outline-none resize-none"
+              name="input-text"
+              id="input-text"
+              placeholder={
+                originLanguage === 'es' ? 'Escribe el texto a traducir...' : 'Ehai pe jehaipyre rembohasa hagua...'
+              }
+              value={originText}
+              onChange={(e) => {
+                setOriginText(e.target.value)
+                debouncedChange(e.target.value)
+              }}
+            ></textarea>
+            <div>
+              <hr className="border-gray-200 mb-2" />
+              <div className="flex items-center justify-between w-full">
+                <p className="text-sm">
+                  <strong>{originText.length}</strong> / 5.000
+                </p>
+                <button>
+                  <Image
+                    src="/icons/copy.svg"
+                    height={24}
+                    width={24}
+                    alt="Copy to Clipboard"
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          {/* Destination Text */}
+          <div className="flex flex-col gap-4 p-6 bg-white border-[1px] border-gray-300 rounded-2xl">
+            <textarea
+              className="h-32 sm:h-48 focus:outline-none resize-none"
+              name="output-text"
+              id="output-text"
+              placeholder={originLanguage === 'es' ? 'Traducción' : 'Ñembohasaha'}
+              readOnly
+              value={translatedText}
+            ></textarea>
+            <div>
+              <hr className="border-gray-200 mb-2" />
+              <div className="flex items-center justify-between w-full">
+                <p className="text-sm">
+                  <strong>{translatedText.length}</strong> / 5.000
+                </p>
+                <button>
+                  <Image
+                    src="/icons/copy.svg"
+                    height={24}
+                    width={24}
+                    alt="Copy to Clipboard"
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Translation Button */}
+        {/* <div>
+          <button
+            onClick={() => translate(originText, originLanguage)}
+            className="bg-black text-white py-2 px-4 rounded-full"
+          >
+            Traducir
+          </button>
+        </div> */}
+      </section>
     </main>
-  );
+  )
 }
